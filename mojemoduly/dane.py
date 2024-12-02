6145e0fd8bd64bab511379488fdc46d95842e736
albim72 <marcin.albiniak72@gmail.com>
2024-12-02 15:19:31 +0100

    Create dane.py

---
+++ mojemoduly/dane.py
@@ -0,0 +1,10 @@
+nrfilii = [2,4,15,46,78,36,73,87,32]
+
+book = {
+    "tytuł":"ABC biegacza ultra",
+    "autor":"Andrzej Ricek",
+    "cena":56.6,
+    "wydawnictwo":"RUN",
+    "liczba stron":489,
+    "nr filii":nrfilii
+}
