96d05344ea80ecd42afbf900af4d26acb7a08418
albim72 <marcin.albiniak72@gmail.com>
2024-12-02 15:47:00 +0100

    Update mojafunkcja.py

--- mojemoduly/funkcje/mojafunkcja.py
+++ mojemoduly/funkcje/mojafunkcja.py
@@ -2,8 +2,8 @@ def czytaj_liste(lista):
     print(f"______ lista: {lista} _____")
     for i,j in enumerate(lista):
         print(f"element listy nr {i+1} -> wartość: {j}")
-        
+
 def czytaj_slownik(slownik):
     print(f"_____ słownik {slownik} _____")
-    for x,y in slownik.iter():
+    for x,y in slownik.items():
         print(f"klucz: {x} -> wartość: {y}")
